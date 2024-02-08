0b94ff8f2c4c7de5ea1789bb4e2ade43471dbbd5
bilalceng <bilalberekgm@gmail.com>
2024-02-08 22:51:36 +0300

    adding Buffer(), Conflate()

--- app/src/main/java/com/bilalberekgm/kotlinflows/FlowsViewModel.kt
+++ app/src/main/java/com/bilalberekgm/kotlinflows/FlowsViewModel.kt
@@ -1,7 +1,5 @@
 package com.bilalberekgm.kotlinflows
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
@@ -10,6 +8,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flow
@@ -46,7 +47,7 @@ class FlowsViewModel():ViewModel() {
 
         }.flowOn(Dispatchers.IO)
 
-
+    //<-----------------------------Flow terminal operators----------------------->
     /**
      *   init {
      *         collectFlow()
@@ -88,43 +89,188 @@ class FlowsViewModel():ViewModel() {
      *      */
      */
 
+    //<--------------------------FlatMapMerge---------------------------->
+    /**
+     *  val remoteRepo: Map<Int ,String> = mapOf(
+     *         Pair(1,"bilal"),
+     *         Pair(2,"yusuf"),
+     *         Pair(3,"zeynep"),
+     *         Pair(4,"rabia"),
+     *         Pair(5,"bedia")
+     *     )
+     *
+     *     //flatMapConcat is used to concatinate two sequential call.
+     *     val flow1 = flow {
+     *         (1..5).forEach {
+     *             delay(200L)
+     *             println("Getting  id of first user: $it")
+     *             emit(it)
+     *         }
+     *     }
+     *
+     *
+     *     private fun fetchUserById(): Flow<String>{
+     *         return flow1.flatMapConcat { userId ->
+     *             flow {
+     *                 delay(2000L)
+     *                 println("getting $userId.th user")
+     *                 emit(remoteRepo[userId]!!)
+     *             }
+     *         }
+     *     }
+     *     init {
+     *         collectFlatMap()
+     *     }
+     *     private fun collectFlatMap(){
+     *         viewModelScope.launch(Dispatchers.IO) {
+     *             fetchUserById().collect{ user ->
+     *             _flatMapFlowData.value = user
+     *             }
+     *         }
+     *     }
+     *
+     *
+     */
 
+    /**
+     *
+     *     val remoteRepo: Map<Int ,String> = mapOf(
+     *         Pair(1,"bilal"),
+     *         Pair(2,"yusuf"),
+     *         Pair(3,"zeynep"),
+     *         Pair(4,"rabia"),
+     *        Pair(5,"bedia")
+     *     )
+     *
+     *     //flatMapMerge first emit all userIds and then fetch users from remote repo.
+     *     val flow1 = flow {
+     *         (1..5).forEach {
+     *             delay(200L)
+     *             println("Getting  id of first user: $it")
+     *             emit(it)
+     *         }
+     *     }
+     *
+     *
+     *     private fun fetchUserById(): Flow<String>{
+     *         return flow1.flatMapMerge { userId ->
+     *             flow {
+     *                 delay(2000L)
+     *                 println("getting $userId.th user")
+     *                 emit(remoteRepo[userId]!!)
+     *             }
+     *         }
+     *     }
+     *     init {
+     *         collectFlatMap()
+     *     }
+     *     private fun collectFlatMap(){
+     *         viewModelScope.launch(Dispatchers.IO) {
+     *             fetchUserById().collect{ user ->
+     *                 _flatMapFlowData.value = user
+     *             }
+     *         }
+     *     }
+     *
+     */
+        //<-----------------------------------FlatmapLatest--------------------------->
+    /**
+     *     val remoteRepo: Map<Int ,String> = mapOf(
+     *         Pair(1,"bilal"),
+     *         Pair(2,"yusuf"),
+     *         Pair(3,"zeynep"),
+     *         Pair(4,"rabia"),
+     *         Pair(5,"bedia")
+     *     )
+     *
+     *     //flatMapMerge first emit all userIds and then fetch users from remote repo.
+     *     val flow1 = flow {
+     *         (1..5).forEach {
+     *             //fetching userId time
+     *             delay(200L)
+     *             println("Getting  id of first user: $it")
+     *             emit(it)
+     *         }
+     *     }
+     *
+     *     // If fetching user time < fetching userId it  acts like flatmapMerge. Otherwise it will fetch only the latest user.
+     *     private fun fetchUserById(): Flow<String>{
+     *         return flow1.flatMapLatest { userId ->
+     *             flow {
+     *                 //fetching user time
+     *                 delay(20L)
+     *                 println("getting $userId.th user")
+     *                 emit(remoteRepo[userId]!!)
+     *             }
+     *         }
+     *     }
+     *     init {
+     *         collectFlatMap()
+     *     }
+     *     private fun collectFlatMap(){
+     *         viewModelScope.launch(Dispatchers.IO) {
+     *             fetchUserById().collect { user ->
+     *                 _flatMapFlowData.value = user
+     *             }
+     *         }
+     *     }
+     */
+    //<----------------------------Buffer()--------------------------->
+    /**
+     *   init {
+     *         giveOrder()
+     *     }
+     *     private  fun giveOrder(){
+     *         val flow = flow<String>{
+     *             delay(250L)
+     *                 emit("appetizer")
+     *                 emit("MainDish")
+     *                 emit("dessert")
+     *             }
+     *         //with buffer we  all values and  collect them at the same time .
+     *         // this behaviour happen cause of the
+     *         // different coroutine is used before and after  buffer() operator
+     *         viewModelScope.launch {
+     *             flow.onEach {
+     *                 println("Flow: $it delivered")
+     *             }.buffer()
+     *                 .collect{
+     *                 println("Flow: now eating $it")
+     *                 delay(2000L)
+     *                 println("Flow: finished eating $it")
+     *             }
+     *         }
+     *         }
+     *
+     */
 
-
-    val remoteRepo: Map<Int ,String> = mapOf(
-        Pair(1,"bilal"),
-        Pair(2,"yusuf"),
-        Pair(3,"zeynep"),
-        Pair(4,"rabia"),
-        Pair(5,"bedia")
-    )
-
-    //flatMapConcat is used to concatinate two sequential call.
-    val flow1 = flow {
-        (1..5).forEach {
-            delay(200L)
-            println("Getting  id of first user: $it")
-            emit(it)
-        }
-    }
-
-    private fun fetchUserById(): Flow<String>{
-        return flow1.flatMapConcat { userId ->
-            flow {
-                delay(2000L)
-                println("getting $userId.th user")
-                emit(remoteRepo[userId]!!)
-            }
-        }
-    }
     init {
-        collectFlatMap()
+        giveOrder()
     }
-    private fun collectFlatMap(){
-        viewModelScope.launch(Dispatchers.IO) {
-            fetchUserById().collect{ user ->
-            _flatMapFlowData.value = user
-            }
+    // With conflate operator we collect  first emition and take last emitted value.
+    private  fun giveOrder(){
+        val flow = flow{
+
+            emit("appetizer")
+            emit("MainDish")
+            emit("dessert")
+            emit("kebab")
+            emit("soup")
+            emit("tea")
+        }
+        //with buffer we  all values and  collect them at the same time .
+        // this behaviour happen cause of the
+        // different coroutine is used before and after  buffer() operator
+        viewModelScope.launch {
+            flow.onEach {
+                println("Flow: $it delivered")
+            }.conflate()
+                .collect{
+                    println("Flow: now eating $it")
+
+                    println("Flow: finished eating $it")
+                }
         }
     }
+
 }
